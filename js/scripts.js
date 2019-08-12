736d8834809a10de9390919595e26c39bf5c6a94
Erin Deuel and Loewy Malkovich <loewy@>
2019-08-12 14:42:57 -0700

    create folders, add jquery and scripts to alert user of which elements he is clickin on

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+  $('h1').click(function() {
+    alert('This is a header.');
+    alert('I told you, THIS IS A HEADER! GET WITH IT!!!!');
+  });
+
+  $('p').click(function() {
+    alert('This is a paragraph.');
+  });
+
+  $('img').click(function() {
+    alert('This is an image of a barrel eye fish.');
+  });
+});
